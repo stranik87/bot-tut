25aac6f0f69f6532dbc4da4a420e401c3dc96853
djumanov <diyorbekjumanov1120@gmail.com>
2023-05-17 17:50:54 +0500

    init

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 import requests
 
-response = requests.get('https://api.telegram.org/bot6203711973:AAHo-eqpnqHk0MCDLy8nLcZBfuBa5ysHAow/getMe')
+TOKEN = '6203711973:AAHo-eqpnqHk0MCDLy8nLcZBfuBa5ysHAow'
+
+url = f'https://api.telegram.org/bot{TOKEN}/getMe'
+
+response = requests.get()
 
 print(response.json())
